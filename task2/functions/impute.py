215c323791f5199d095963e8e2c1c13fa1c2d08d
edinmsa <si7660da-s@student.lu.se>
2022-04-12 22:24:56 +0200

    add return of imputer objects

--- task2/functions/impute.py
+++ task2/functions/impute.py
@@ -10,7 +10,8 @@ def impute(df: pd.DataFrame, threshold: float, max_iter: int) -> pd.DataFrame:
     Note that mutlivariate imputation takes very long to converge; you'll likely want to 
     perform it once and save it to disk."""
 
-    df_single = single_impute(df, threshold) 
-    df_multi = multivariate_impute(df_single, max_iter=max_iter)
+    df_single, single_imputers = single_impute(df, threshold) 
+    df_multi, multi_imputer = multivariate_impute(df_single, max_iter=max_iter)
 
-    return df_multi
+    # You'll need the imptuter objects when imputing any new data, e.g. the test set 
+    return df_multi, (single_imputers, multi_imputer)
