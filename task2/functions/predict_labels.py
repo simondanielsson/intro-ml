988f4798ebc6b0ff19c9cacbf195a5898281d8ca
zixinhu15 <102033419+zixinhu15@users.noreply.github.com>
2022-04-22 23:41:15 +0200

    Correct predict_proba, choose only col 1.

--- task2/functions/predict_labels.py
+++ task2/functions/predict_labels.py
@@ -13,7 +13,7 @@ def predict_labels(best_models: List[Tuple[str, object]], subtask: int, X_test) 
         label_name = best_models[i][0]
         
         if subtask == 1 or subtask == 2:
-            label_p = best_models[i][1].predict_proba(X_test)
+            label_p = best_models[i][1].predict_proba(X_test)[1]
         else:
             label_p = best_models[i][1].predict(X_test)
             
