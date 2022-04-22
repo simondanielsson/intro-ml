7da76369c8e94412213309003e1c0628001d7776
zixinhu15 <102033419+zixinhu15@users.noreply.github.com>
2022-04-22 18:11:08 +0200

    Update predict_labels.py

--- task2/functions/predict_labels.py
+++ task2/functions/predict_labels.py
@@ -3,6 +3,15 @@ from typing import List, Tuple
 import pandas as pd
 
 def predict_labels(best_models: List[Tuple[str, object]], subtask: int, X_test) -> pd.DataFrame:
-    """Takes the best model and predicts the probabilities on the test set on the label, 
-    outputs a resulting predicted dataframe with all the label columns"""
-    pass
+    """
+    Takes the best model and predicts the probabilities on the test set on the label, 
+    outputs a resulting predicted dataframe with all the label columns
+    """
+    labels_pred = pd.DataFrame()
+
+    for i in range(len(best_models)):
+        label_name = best_models[i][0]
+        label_p = best_models[i][1]
+        labels_pred[label_name] = label_p    
+    
+    return labels_pred
