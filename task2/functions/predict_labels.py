6fe99ea7dfd10c932a849e550a0992347a87923a
zixinhu15 <102033419+zixinhu15@users.noreply.github.com>
2022-04-23 12:50:30 +0200

    Use copy.copy() when assigning value for label_p

--- task2/functions/predict_labels.py
+++ task2/functions/predict_labels.py
@@ -13,10 +13,10 @@ def predict_labels(best_models: List[Tuple[str, object]], subtask: int, X_test) 
         label_name = best_models[i][0]
         
         if subtask == 1 or subtask == 2:
-            label_p = best_models[i][1].predict_proba(X_test)[1]
+            label_p = copy.copy(best_models[i][1].predict_proba(X_test)[:, 1])
         else:
-            label_p = best_models[i][1].predict(X_test)
+            label_p = copy.copy(best_models[i][1].predict(X_test))
             
-        labels_pred[label_name] = label_p   
+        labels_pred[label_name] = label_p   # there is no binding between labels_pred[label_name] and label_p
     
     return labels_pred
